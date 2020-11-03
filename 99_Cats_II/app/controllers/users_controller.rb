93f9197e3cbaf93336d2085cc77c52b246d2a1a8
joshkohane <joshkohane@gmail.com>
2020-11-02 19:55:38 -0500

    Create Auth for 99 Cats App

---
+++ 99_Cats_II/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to cats_url
+        else
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:user_name, :password)
+    end
+end
